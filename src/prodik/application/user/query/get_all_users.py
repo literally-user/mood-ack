47f30fcdd21a5aad5e25c7d7b57ddd939704d344
Davydov Davlat <literally_user@hotmail.com>
2026-04-13 10:36:37 +0300

    feat(application): get concrete user & get all users interactors

---
+++ src/prodik/application/user/query/get_all_users.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from prodik.application.interfaces.identity_provider import IdentityProvider
+from prodik.application.interfaces.repositories import UserRepository
+from prodik.application.errors import NotEnoughRightsError
+from prodik.domain.user import User
+
+@dataclass
+class GetAllUsersInteractor:
+    user_repository: UserRepository
+    idp: IdentityProvider
+
+    async def execute(self, page: int, size: int) -> list[User]:
+        current_user = await self.idp.get_current_user()
+        if not current_user.can_manage_users():
+            raise NotEnoughRightsError("Not enough rights to perform operation")
+
+        return await self.user_repository.get_all(page, size)
---
+++ src/prodik/application/user/query/get_user_profile.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from prodik.application.interfaces.repositories import UserRepository
+from prodik.application.errors import UserNotFoundError
+from prodik.domain.user import UserId, User
+
+
+@dataclass
+class GetUserProfileInteractor:
+    user_repository: UserRepository
+
+    async def execute(self, target_id: UserId) -> User:
+        user = await self.user_repository.get_by_uuid(target_id)
+        if user is None:
+            raise UserNotFoundError("User not found")
+        
+        return user
