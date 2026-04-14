77a7adc3f6b6280295fc91a4413ffc65492f79ce
Davydov Davlat <literally_user@hotmail.com>
2026-04-14 10:34:49 +0300

    feat(bootstrap): link implementations

--- src/prodik/bootstrap/di/providers/infrastructure.py
+++ src/prodik/bootstrap/di/providers/infrastructure.py
@@ -1,6 +1,13 @@
 from dishka import Provider, Scope, WithParents, provide_all
 
 from prodik.infrastructure.password_hasher import PasswordHasherImpl
+from prodik.infrastructure.repositories import (
+    LocalAuthorizationRepositoryImpl,
+    OAuthAuthorizationRepositoryImpl,
+    TaskRepositoryImpl,
+    UserRepositoryImpl,
+    UserSessionRepositoryImpl,
+)
 from prodik.infrastructure.token_manager import (
     AccessTokenManagerImpl,
     OAuthTokenManagerImpl,
@@ -12,6 +19,11 @@ from prodik.infrastructure.transaction_manager import TransactionManagerImpl
 
 class InfrastructureProvider(Provider):
     provides = provide_all(
+        WithParents[OAuthAuthorizationRepositoryImpl],
+        WithParents[LocalAuthorizationRepositoryImpl],
+        WithParents[UserSessionRepositoryImpl],
+        WithParents[UserRepositoryImpl],
+        WithParents[TaskRepositoryImpl],
         WithParents[PasswordHasherImpl],
         WithParents[AccessTokenManagerImpl],
         WithParents[RefreshTokenManagerImpl],
